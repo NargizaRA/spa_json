fb1dde428d6427be2f6a275db86f7b31f19e181e
NargizaRA <rysmendieva.nargiza@gmail.com>
2022-07-27 10:51:07 +0200

    solution

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,32 @@
-// \/ All of your javascript should go here \/
+import { booksData } from "./data.js";
+
+let parsedData = JSON.parse(booksData)
+
+function createHTMLCards (booksArray) {
+  
+  return booksArray.map(books=>{
+    return  (
+      `<div class="card m-4" style="width: 15rem;">
+        <img src="${books.image}" class="card-img-top" alt="books" height="200rem">
+        <div class="card-body">
+          <h5 class="card-title">${books.title}</h5>
+          <div class="d-flex align-items-center justify-content-between" >
+          <h6>By:${books.author}</h6>
+          </div>
+          <p class="border-top border-secondary pt-2">Categories: ${books.categories.join(", ")}</p>
+        </div>
+      </div>`
+    )
+  }).join('')
+}
+
+
+function renderSections () {
+  const sections = ["classics","detectives","fantasy","romance"]
+  sections.forEach(sectionID => {
+    const section = document.getElementById(sectionID);
+    section.innerHTML = createHTMLCards(parsedData[sectionID]) 
+  })
+}
+
+renderSections()
